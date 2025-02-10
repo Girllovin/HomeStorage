85d5e67cb935a7a9033f2b420352214969d39485
Girllovin <gerllovin@gmail.com>
2025-02-10 23:48:13 +0200

    Controller update 2

--- HomeStoragePrj/src/main/java/smallITgroup/appl/SavingData.java
+++ HomeStoragePrj/src/main/java/smallITgroup/appl/SavingData.java
@@ -13,6 +13,8 @@ import java.io.IOException;
 public class SavingData {
 	static ObjectMapper mapper = new ObjectMapper();
 
+	// saving whole storage system into the file "target.Storage.json"
+	// serialization
 	public static boolean saveStorageData(HomeStorageDto storage) {
 		try {
 			mapper.registerModule(new JavaTimeModule());
@@ -30,6 +32,39 @@ public class SavingData {
 		return true;
 	}
 
+	// saving item into the file "itemId.json"
+	// serialization
+	public static boolean saveItemData(ItemDto item) {
+		mapper.registerModule(new JavaTimeModule());
+		mapper.enable(SerializationFeature.INDENT_OUTPUT);
+		try {
+			File file = new File(item.getId() + ".json");
+			mapper.writeValue(file, item);
+			System.out.println("Item data saved successfully.");
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+
+	// saving shelf into the file "shelfId.json"
+	// serialization
+	public static boolean saveShelfData(ShelfDto shelf) {
+		mapper.registerModule(new JavaTimeModule());
+		mapper.enable(SerializationFeature.INDENT_OUTPUT);
+		try {
+			File file = new File(shelf.getId() + ".json");
+			mapper.writeValue(file, shelf);
+			System.out.println("Shelf data saved successfully.");
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+
+	
 	public static ShelfDto restoreShelf(File file) {
 		try {
 
@@ -48,34 +83,6 @@ public class SavingData {
 			e.printStackTrace();
 			return null;
 		}
-	}
-
-	public static boolean saveItemData(ItemDto item) {
-		mapper.registerModule(new JavaTimeModule());
-		mapper.enable(SerializationFeature.INDENT_OUTPUT);
-		try {
-			File file = new File(item.getId() + ".json");
-			mapper.writeValue(file, item);
-			System.out.println("Item data saved successfully.");
-		} catch (IOException e) {
-			e.printStackTrace();
-			return false;
-		}
-		return true;
-	}
-
-	public static boolean saveShelfData(ShelfDto shelf) {
-		mapper.registerModule(new JavaTimeModule());
-		mapper.enable(SerializationFeature.INDENT_OUTPUT);
-		try {
-			File file = new File(shelf.getId() + ".json");
-			mapper.writeValue(file, shelf);
-			System.out.println("Shelf data saved successfully.");
-		} catch (IOException e) {
-			e.printStackTrace();
-			return false;
-		}
-		return true;
 	}
 
 }
