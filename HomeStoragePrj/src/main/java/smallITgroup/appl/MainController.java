8f66b351584edeb1e09e7568ffcff3e18f9bae85
Girllovin <gerllovin@gmail.com>
2025-02-15 19:11:31 +0200

    Merge remote-tracking branch 'origin/main'

--- HomeStoragePrj/src/main/java/smallITgroup/appl/MainController.java
+++ HomeStoragePrj/src/main/java/smallITgroup/appl/MainController.java
@@ -5,7 +5,9 @@ import java.io.File;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.InputMismatchException;
 import java.util.List;
+import java.util.Scanner;
 import java.util.Set;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -25,59 +27,59 @@ public class MainController {
 	
 	public static ObjectMapper mapper = new ObjectMapper();
 	
-	public static void main(String[] args) {		
-       
-//		lineCounter++;
-//		ItemDto item1 = new ItemDto("First item","Ski", "Sport", 10.12, 6.15, LocalDateTime.now()) ;
-//		SavingData.saveItemData(item1);
-//		ItemDto item2 = new ItemDto("Second item", "Watermelon", "Food", 1.05, 2.10, LocalDateTime.now()) ;
-//		SavingData.saveItemData(item2);
-//		ItemDto item3 = new ItemDto("Third item","Hammer", "Instruments", 17.12, 8.17, LocalDateTime.now()) ;
-//		SavingData.saveItemData(item3);
-//		Set<ItemDto> items = new HashSet<ItemDto>();
-//		items.add(item1);
-//		items.add(item2);
-//		items.add(item3);
-//		
-//		List<String> warnings = new ArrayList<String>();
-//		
-//		
-//		ShelfDto shelf1 = new ShelfDto("Shelf" + ++counterShelf, "Everything", items, 500, 500, 100, 0, false, warnings );
-//		SavingData.saveShelfData(shelf1);
-//		
-//		ItemDto item4 = new ItemDto("Forth item","Scooter", "Sport", 40.12, 11.15, LocalDateTime.now()) ;
-//		SavingData.saveItemData(item4);
-//		ItemDto item5 = new ItemDto("Fifth item", "Bread", "Food", 0.5, 1.10, LocalDateTime.now()) ;
-//		SavingData.saveItemData(item5);
-//		ItemDto item6 = new ItemDto("Sixth item","Skateboard", "Sport", 7.12, 4.07, LocalDateTime.now()) ;
-//		SavingData.saveItemData(item6);
-//		Set<ItemDto> items2 = new HashSet<ItemDto>();
-//		items2.add(item4);
-//		items2.add(item5);
-//		items2.add(item6);
-//		
-//		ShelfDto shelf3 = new ShelfDto("Shelf" + ++counterShelf, "Everything", items2, 500, 500, 100, 0, false, warnings );
-//		SavingData.saveShelfData(shelf3);
-//		
-//		List<ShelfDto> shelfs = new ArrayList<ShelfDto>();
-//		shelfs.add(shelf1);
-//		shelfs.add(shelf3);
-//		HomeStorageDto homeStorage = new HomeStorageDto("Our storage room", warnings, shelfs, 2000, 0, false, warnings);
-//		SavingData.saveStorageData(homeStorage);
-//		
-//		
-//		File file = new File("Shelf1.json");
-//		File file2 = new File("Shelf2.json");
-//		File file3 = new File("target.Storage.json");
-//		
-//		ShelfDto shelf2 = SavingData.restoreShelf(file);
-//		System.out.println(shelf2);
-//		ShelfDto shelf4 = SavingData.restoreShelf(file2);
-//		System.out.println(shelf4);
-//		HomeStorageDto homeStorage1 = SavingData.restoreStorage(file3);
-//		System.out.println(homeStorage1);
-//		
-	}
-	
-
+	public static void main(String[] args) {	
+		lineCounter++;		
+		ItemDto item1 = new ItemDto("First item", "Chair", "Furniture", "A2", 2.2, 1.7, LocalDateTime.now()) ;
+		SavingDataController.saveItemData(item1);
+		ItemDto item2 = new ItemDto("Second item", "Watermelon", "Food", "A1", 1.05, 2.10, LocalDateTime.now()) ;
+		SavingDataController.saveItemData(item2);
+		ItemDto item3 = new ItemDto("Third item","Hammer", "Instruments", "A1", 17.12, 8.17, LocalDateTime.now()) ;
+		SavingDataController.saveItemData(item3);
+		Set<ItemDto> items = new HashSet<ItemDto>();
+		items.add(item1);
+		items.add(item2);
+		items.add(item3);
+		
+		List<String> warnings = new ArrayList<String>();
+		
+		
+		ShelfDto shelf1 = new ShelfDto("Shelf" + ++shelfCounter, "Everything", items, 500.0, 500.0, 100.0, 0.0, false, warnings );
+		SavingDataController.saveShelfData(shelf1);
+		
+		ItemDto item4 = new ItemDto("Forth item","Scooter", "Sport", "A1", 40.12, 11.15, LocalDateTime.now()) ;
+		SavingDataController.saveItemData(item4);
+		ItemDto item5 = new ItemDto("Fifth item", "Bread", "Food", "A1", 0.5, 1.10, LocalDateTime.now()) ;
+		SavingDataController.saveItemData(item5);
+		ItemDto item6 = new ItemDto("Sixth item","Skateboard", "Sport", "A1", 7.12, 4.07, LocalDateTime.now()) ;
+		SavingDataController.saveItemData(item6);
+		Set<ItemDto> items2 = new HashSet<ItemDto>();
+		items2.add(item4);
+		items2.add(item5);
+		items2.add(item6);
+		
+		ShelfDto shelf3 = new ShelfDto("Shelf" + ++shelfCounter, "Everything", items2, 500, 500, 100, 0, false, warnings );
+		SavingDataController.saveShelfData(shelf3);
+		
+		List<ShelfDto> shelfs = new ArrayList<ShelfDto>();
+		shelfs.add(shelf1);
+		shelfs.add(shelf3);
+		HomeStorageDto homeStorage = new HomeStorageDto("Our storage room", warnings, shelfs, 2000, 0, false, warnings);
+		SavingDataController.saveStorageData(homeStorage);
+		
+		
+		File file = new File("Shelf1.json");
+		File file2 = new File("Shelf2.json");
+		File file3 = new File("target.Storage.json");
+		
+		ShelfDto shelf2 = SavingDataController.restoreShelf(file);
+		System.out.println(shelf2);
+		ShelfDto shelf4 = SavingDataController.restoreShelf(file2);
+		System.out.println(shelf4);
+		HomeStorageDto homeStorage1 = SavingDataController.restoreStorage(file3);
+		System.out.println(homeStorage1);	
+		
+//		Menu option
+		MenuOptions menu = new MenuOptions();
+		menu.menu();
+	}	
 }
---
+++ HomeStoragePrj/src/main/java/smallITgroup/appl/MenuOptions.java
@@ -0,0 +1,157 @@
+package smallITgroup.appl;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class MenuOptions {
+	static Scanner scanner = new Scanner(System.in);
+	static StorageController controller = new StorageController();
+	public static void menu(){		
+		while (true) {
+			chooseOptions();
+			int choice;
+			try {
+				choice = scanner.nextInt();
+			} catch(InputMismatchException e) {
+				System.err.println("Input Error! Try again.");
+				scanner.nextLine(); 
+				continue;
+			}		
+	        switch (choice) {
+	            case 1:
+	            	monitorStorageMenu();
+	                break;
+	            case 2:     
+	            	searchMenu();
+	                break;
+	            case 3:
+	            	manageShelvesMenu();
+	                break;
+	            case 4:
+	            	manageProductMenu();
+	                break;
+	            case 5:
+	            	organizeStorage();
+	                break;
+	            case 6:   
+//	            	TODO Input storage name
+	            	controller.checkGlobalWarnings(null);
+	                break;
+
+	            case 0:
+	            	System.out.println("__________________\nApplication closed");
+	                scanner.close();
+	                System.exit(0);
+	                return;
+	            default:
+	                System.out.println("\nInvalid choice. Please try again.\n");
+	        }
+		}	
+	}	
+
+	private static void organizeStorage() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void manageProductMenu() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void manageShelvesMenu() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void searchMenu() {
+		System.out.println("""
+				1. Search by Name
+				2. Search by ID
+
+				0. Main menu
+				""");
+		while (true) {
+			int choice;
+			try {
+				choice = scanner.nextInt();
+			} catch(InputMismatchException e) {
+				System.err.println("Input Error! Try again.");
+				scanner.nextLine(); 
+				continue;
+			}		
+	        switch (choice) {
+	            case 1:  
+	            	System.out.println("Input product name\n");
+	            	String itemName = scanner.next();
+	            	controller.itemNameSearch(itemName);
+	            	break;
+	            case 2:   
+	            	System.out.println("Input product ID\n");
+	            	String itemId = scanner.next();
+	            	controller.itemIdSearch(itemId);
+	            	break;
+	            case 0:
+	            	System.out.println("Return to the main menu\n");
+	                return;
+	            default:
+	                System.out.println("\nInvalid choice. Please try again.\n");
+	        }
+		}
+		
+	}
+
+	private static void monitorStorageMenu() {
+		System.out.println("""
+				1. Show Items on the shelf
+				2. Show full storage
+
+				0. Main menu
+				""");
+		while (true) {
+			int choice;
+			try {
+				choice = scanner.nextInt();
+			} catch(InputMismatchException e) {
+				System.err.println("Input Error! Try again.");
+				scanner.nextLine(); 
+				continue;
+			}		
+	        switch (choice) {
+	            case 1:
+	            	System.out.println("Input shelf number: ");
+	            	String shelfNumber = scanner.next();
+	            	
+//	            	TODO Checking if the shelf does exist
+	            	controller.showItemsOnTheShelf(shelfNumber);	            	            	
+	            	return;
+	            case 2:   
+//	            	TODO Correct storageName
+	            	String storageName = "Default name";
+	            	controller.showItemsInTheStorage(storageName);
+	            	return;
+
+	            case 0:
+	            	System.out.println("Return to the main menu\n");
+	                return;
+	            default:
+	                System.out.println("\nInvalid choice. Please try again.\n");
+	        }
+		}
+	}
+	private static void chooseOptions() {
+		System.out.println("""
+				||Home Storage Menegement System (HSMS)||
+
+				1. Inventory View
+				2. Search
+				3. Manage Shelves
+				4. Product Management
+				5. Organize Storage
+				6. Expiration Date Tracking
+				
+				0. Exit the application
+				""");
+		
+	}
+}
